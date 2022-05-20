91daecd2dcbea2ed73d5c55b88ce6ef818a379ed
dlgusdn978 <dlgusdn978@naver.com>
2022-05-20 12:46:21 +0900

    2022/05/20

---
+++ SwExpertAcademy/src/d1/AverageValue.java
@@ -0,0 +1,23 @@
+package d1;
+
+import java.util.Scanner;
+
+public class AverageValue {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			double sum =0;
+			for(int i=0; i<10; i++) {
+				T=sc.nextInt();
+				sum += T;
+			}
+			System.out.println("#"+test_case+" "+Math.round(sum/10));
+		}
+	}
+
+}
